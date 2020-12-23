110454697800def8c182f0a53df33be2f6870956
Aashish <pawarashish564@gmail.com>
2020-12-23 13:08:50 +0530

    💥 NEW: remove_outliers_using_quantiles method

--- mlutil.py
+++ mlutil.py
@@ -192,3 +192,59 @@ def print_quantile_info(qu_dataset, qu_field):
     print("percentage of records out of outer fences: %.2f"% (percentage))
 
 
+# Function: remove_outliers_using_quantiles(qu_dataset, qu_field, qu_fence)
+#   1- Remove outliers according to the given fence value and return new dataframe.
+#   2- Print out the following information about the data
+#      - interquartile range
+#      - upper_inner_fence
+#      - lower_inner_fence
+#      - upper_outer_fence
+#      - lower_outer_fence
+#      - percentage of records out of inner fences
+#      - percentage of records out of outer fences
+# Input: 
+#   - pandas dataframe (qu_dataset)
+#   - name of the column to analyze (qu_field)
+#   - inner (1.5*iqr) or outer (3.0*iqr) (qu_fence) values: "inner" or "outer"
+# Output:
+#   - new pandas dataframe (output_dataset)
+
+def remove_outliers_using_quantiles(qu_dataset, qu_field, qu_fence):
+    a = qu_dataset[qu_field].describe()
+    
+    iqr = a["75%"] - a["25%"]
+    print("interquartile range:", iqr)
+    
+    upper_inner_fence = a["75%"] + 1.5 * iqr
+    lower_inner_fence = a["25%"] - 1.5 * iqr
+    print("upper_inner_fence:", upper_inner_fence)
+    print("lower_inner_fence:", lower_inner_fence)
+    
+    upper_outer_fence = a["75%"] + 3 * iqr
+    lower_outer_fence = a["25%"] - 3 * iqr
+    print("upper_outer_fence:", upper_outer_fence)
+    print("lower_outer_fence:", lower_outer_fence)
+    
+    count_over_upper = len(qu_dataset[qu_dataset[qu_field]>upper_inner_fence])
+    count_under_lower = len(qu_dataset[qu_dataset[qu_field]<lower_inner_fence])
+    percentage = 100 * (count_under_lower + count_over_upper) / a["count"]
+    print("percentage of records out of inner fences: %.2f"% (percentage))
+    
+    count_over_upper = len(qu_dataset[qu_dataset[qu_field]>upper_outer_fence])
+    count_under_lower = len(qu_dataset[qu_dataset[qu_field]<lower_outer_fence])
+    percentage = 100 * (count_under_lower + count_over_upper) / a["count"]
+    print("percentage of records out of outer fences: %.2f"% (percentage))
+    
+    if qu_fence == "inner":
+        output_dataset = qu_dataset[qu_dataset[qu_field]<=upper_inner_fence]
+        output_dataset = output_dataset[output_dataset[qu_field]>=lower_inner_fence]
+    elif qu_fence == "outer":
+        output_dataset = qu_dataset[qu_dataset[qu_field]<=upper_outer_fence]
+        output_dataset = output_dataset[output_dataset[qu_field]>=lower_outer_fence]
+    else:
+        output_dataset = qu_dataset
+    
+    print("length of input dataframe:", len(qu_dataset))
+    print("length of new dataframe after outlier removal:", len(output_dataset))
+    
+    return output_dataset
